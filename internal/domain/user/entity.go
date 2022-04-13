19ba2dd3f4ffb2b5c1b5b76a367a55ddccda8bd3
Burak Atak <atak_burak_75@hotmail.com>
2022-04-13 16:01:31 +0300

    User' default role updated

--- internal/domain/user/entity.go
+++ internal/domain/user/entity.go
@@ -8,5 +8,5 @@ type User struct {
 	gorm.Model
 	Email    string `json:"email" validate:"email"`
 	Password string `json:"password" validate:"min=8,max=20"`
-	Role     string `json:"role" gorm:"default:admin"`
+	Role     string `json:"role" gorm:"default:costumer"`
 }
